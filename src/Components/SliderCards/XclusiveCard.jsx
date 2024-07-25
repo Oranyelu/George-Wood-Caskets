7577b8de411d80449e1a7925730ceee1b250a6c0
Oranyelu <georgechime91@gmail.com>
2024-07-25 15:37:53 +0100

    banners update

--- src/Components/SliderCards/XclusiveCard.jsx
+++ src/Components/SliderCards/XclusiveCard.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import XclusiveBanner from '../../assets/svgs/Xclusive.svg'
+
 
 function XclusiveCard() {
   return (
-    <div>XclusiveCard</div>
+    <div className='h-[400px] w-[700px]'>
+      <img src={XclusiveBanner} className='h-[100%]'/>
+    </div>
   )
 }
 

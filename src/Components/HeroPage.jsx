f8858af5bbd6a0d0572393541f293c41408a932a
Oranyelu <georgechime91@gmail.com>
2024-07-25 20:37:44 +0100

    no major update yet except the hero page component

--- src/Components/HeroPage.jsx
+++ src/Components/HeroPage.jsx
@@ -1,24 +1,38 @@
 import React from "react";
 import Weather from "./Weather";
 import Carousel from "./Carousel";
+import HeroMobileComp from "./HeroMobileComp";
 
 function HeroPage() {
   return (
-    <div className="flex h-[100vh] items-center w-[100%]">
-      <main className="flex flex-col justify-center w-[70vw] h-[100%]" id="slides">
-        <Carousel />
-        <article className="pl-7 pt-20">
-          <h1 className=" text-[50px] text-[#A37E2C]">40 Years</h1>
-          <p className=" text-[25px] text-[#FFFFFF]">Established in 1984</p>
-        </article>
-      </main>
-      <aside className="bg-[#A37E2C] w-[100%] h-[100%] flex flex-col justify-center items-center" id="weather-app">
-        <div className="weather-clock">
-          <Weather />
+    <div className="mt-[70px]">
+      <div className="w-[100%] meme block md:hidden ">
+        <HeroMobileComp />
+      </div>
+      <div className="hidden md:block">
+        <div className="flex h-[100vh] items-center w-[100%]">
+          <main
+            className="flex flex-col justify-center w-[70vw] h-[100%]"
+            id="slides"
+          >
+            <Carousel />
+            <article className="pl-7 pt-20">
+              <h1 className=" text-[50px] text-[#A37E2C]">40 Years</h1>
+              <p className=" text-[25px] text-[#FFFFFF]">Established in 1984</p>
+            </article>
+          </main>
+          <aside
+            className="bg-[#A37E2C] w-[100%] h-[100%] flex flex-col justify-center items-center"
+            id="weather-app"
+          >
+            <div className="weather-clock">
+              <Weather />
+            </div>
+            <h1 className="text-[#011309]">Celebrating Life and Legacy</h1>
+            <p className="dynamic-text"></p>
+          </aside>
         </div>
-        <h1 className="text-[#011309]">Celebrating Life and Legacy</h1>
-        <p className="dynamic-text"></p>
-      </aside>
+      </div>
     </div>
   );
 }

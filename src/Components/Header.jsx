30e728bd25a322de29c514ed3002da797e8ad6bb
Oranyelu <georgechime91@gmail.com>
2024-09-15 23:55:29 +0100

    jsut fr safety

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="fixed top-0 left-0 right-0 z-50">
+    <div className="fixed top-0 left-0 right-0 z-50 text-[1rem]">
       <div className="hidden md:block">
         <section className="bg-[#A37E2C] flex justify-between p-2 text-white">
           <p>08143904414</p>
@@ -32,14 +32,14 @@ function Header() {
               <img src={Logo} alt="Logo" className="flex p-4" />{" "}
             </Link>
           </div>
-          <div className="flex text-center items-center text-white font-bold text-[20px]">
-            <h1 className="font-serif">George Wood Casket and Furniture</h1>
+          <div className="flex text-center items-center text-white font-bold">
+            <h1 className="">George Wood Casket and Furniture</h1>
           </div>
         </main>
 
         <div className="flex justify-center items-center">
           <div className="hidden sm:block">
-            <span className=" w-[60vw] flex justify-evenly text-white font-bold">
+            <span className=" w-[60vw] flex justify-evenly text-white font-semibold">
               <Link to="/">HOME</Link>
               <Link to="/products">PRODUCTS</Link>
               <Link to="/services">SERVICES</Link>
@@ -51,7 +51,7 @@ function Header() {
           </div>
           <div className="pr-5 flex items-center gap-5 font-semibold text-white">
             <Link to="/cart" className="flex items-center">
-              Cart
+              CART
             </Link>
           </div>
           <span className="ml-2 block sm:hidden pr-4">

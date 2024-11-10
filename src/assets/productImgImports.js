057cfe578076df9f02847e5f8653a32e8dc02708
Oranyelu <georgechime91@gmail.com>
2024-11-10 20:39:51 +0100

    clean up

---
+++ src/assets/productImgImports.js
@@ -0,0 +1,52 @@
+// productImgImports.js
+
+import EmperorWhite from './productSvgs/EmperorWhite.svg';
+import BalmoralWhite from './productSvgs/BalmoralWhite.svg';
+import OxfordBrown from './productSvgs/OxfordBrown.svg';
+import AvalanchWhiteRosegold from './productSvgs/AvalanchWhite.svg';
+import HavardBrown from './productSvgs/HavardBrown.svg';
+import SenatorBrown from './productSvgs/SenatorBrown.svg';
+import Voyager1 from './productSvgs/Voyager1.svg';
+import CoffinBrown from './productSvgs/CoffinBrown.svg';
+import MilitaryBrown from './productSvgs/MilitaryBrown.svg';
+import HavenBrown from './productSvgs/HavenBrown.svg';
+import Sincerity1 from './productSvgs/Sincerity1.svg';
+import OxfordWhite from './productSvgs/OxfordWhite.svg';
+import CliftonBrown from './productSvgs/CliftonBrown.svg';
+import PromiseBrown from './productSvgs/PromiseBrown.svg';
+import CarnationBrown from './productSvgs/CarnationBrown.svg';
+import SyracuseBrown from './productSvgs/SyracuseBrown.svg';
+import BaileyBrown from './productSvgs/BaileyBrown.svg';
+import DelrayBrown from './productSvgs/DelrayBrown.svg';
+import HartwickBrown from './productSvgs/HartWickBrown.svg';
+import BelmontBrown from './productSvgs/BelmontBrown.svg';
+import BarnettBrown from './productSvgs/BarnettBrown.svg';
+import TheBibleBrown from './productSvgs/TheBibleBrown.svg';
+
+// Exporting them as a named export
+const productImages = {
+  EmperorWhite,
+  BalmoralWhite,
+  OxfordBrown,
+  AvalanchWhiteRosegold,
+  HavardBrown,
+  SenatorBrown,
+  Voyager1,
+  CoffinBrown,
+  MilitaryBrown,
+  HavenBrown,
+  Sincerity1,
+  OxfordWhite,
+  CliftonBrown,
+  PromiseBrown,
+  CarnationBrown,
+  SyracuseBrown,
+  BaileyBrown,
+  DelrayBrown,
+  HartwickBrown,
+  BelmontBrown,
+  BarnettBrown,
+  TheBibleBrown
+};
+
+export default productImages;

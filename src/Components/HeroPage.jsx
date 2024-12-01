192a782e50d0a20473f6d667b01e0f613ad557b5
Oranyelu <georgechime91@gmail.com>
2024-12-01 07:40:15 +0100

    more changes on hero page

--- src/Components/HeroPage.jsx
+++ src/Components/HeroPage.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import HeroMobileComp from "./HeroMobileComp";
-import Background from "../assets/svgs/Background.svg";
+import HeroPageImg from "../assets/svgs/Hero-png.png";
 
 function HeroPage() {
   return (
     <div className="mt-[70px]">
-      <div className="w-[100%] meme block md:hidden bg-[url(https://wallpapercave.com/wp/wp4348559.jpg)] ">
+      <div className="w-[100%] meme block md:hidden bg-white ">
         <HeroMobileComp />
       </div>
       <div className="hidden md:block bg-[#f0c068] w-[100%] h-[700px] pt-7 ">
         <div className="w-full h-full flex justify-center items-center">
           <div className="flex items-center bg-white bg-opacity-10 backdrop-filter backdrop-blur-md border border-white border-opacity-20 p-6 rounded-lg shadow-lg w-[90%] h-[90%]">
-            <main className="flex flex-col justify-end w-[100%] h-[100%]">
+            <main className="flex flex-col justify-end w-[50%] h-[100%]">
               <article className="pl-3 pr-3">
-                <h1 className=" text-[50px] text-[#135B3A] font-bold">40 Years</h1>
+                <h1 className=" text-[50px] text-[#135B3A] font-bold">
+                  40 Years
+                </h1>
                 <p className=" text-[25px] text-[#FFFFFF]">
                   Established in 1984
                 </p>
@@ -30,6 +32,9 @@ function HeroPage() {
                 <p className="dynamic-text text-[19px] h-16"></p>
               </article>
             </main>
+            <aside className="w-[50%] ">
+              <img src={HeroPageImg} alt="" className="h-[600px]"/>
+            </aside>
           </div>
         </div>
       </div>
